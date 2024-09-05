d89287042571d5dd366f1b0c568215bdbe5a007d
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-05 16:04:21 +0530

    Update Soundex.h
    Split sub-functions
    Tried to reduce CCN
    Logic is working
    Fingers crossed

--- Soundex.h
+++ Soundex.h
@@ -5,73 +5,69 @@
 #include <ctype.h>
 #include <string.h>
 
-char getSoundexCode(char c) {
-    char returnValue;
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': returnValue = '1'; break;
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': returnValue = '2'; break;
-        case 'D': case 'T': returnValue = '3'; break;
-        case 'L': returnValue = '4'; break;
-        case 'M': case 'N': returnValue = '5'; break;
-        case 'R': returnValue = '6'; break;
-        default: returnValue = '0'; // For A, E, I, O, U, H, W, Y
+void deleteCharFromStringByIndex(char *inputString, int charIndex) {
+    int len = strlen(inputString);
+    int i;
+    for(i = charIndex; i < len-1; ++i) {
+        inputString[i] = inputString[i+1];
     }
-    return returnValue;
+    inputString[i] = '\0';
 }
 
 void generateSoundex_ReplaceCharacters(const char *name, char *soundex) {
+    char soundexCode[26] = {'0','1','2','3','0','1','2','*','0','2','2','4','5','5','0','1','2','6','2','3','0','1','*','2','0','2'};
     int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    for (int i = 1; i < len; i++) {
-        soundex[i] = getSoundexCode(name[i]);
+    
+    for(int i = 0; i < len; ++i) {
+        soundex[i] = soundexCode[(toupper(name[i]) - 65)];
+    }
+    soundex[len] = '\0';
+}
+
+
+void generateSoundex_SimplifyAdjacentCharacters_CheckForHW(char *soundex) {
+    int len  = strlen(soundex);
+    for (int i = 2; i < len; ++i) {
+        if((soundex[i-1] == '*') && (soundex[i] == soundex[i-2])) {
+            deleteCharFromStringByIndex(soundex,i);
+        }
+    }
+}
+
+void generateSoundex_SimplifyAdjacentCharacters_CheckForRepeatedCharacters(char *soundex) {
+    int len = strlen(soundex);
+    
+    for (int i = 1; i < len; ++i) {
+        if(soundex[i] == soundex[i-1]) {
+            deleteCharFromStringByIndex(soundex,i);
+        }
     }
 }
 
 void generateSoundex_RemoveVowels(char *soundex) {
-    for (int i = 1; i < strlen(soundex); i++) {
-     if(soundex[i] == '0') {
-        soundex[i] = soundex[i-1];
-     }   
-    }
-}
-
-void generateSoundex_SimplifyAdjacentCharacters(char *soundex) {
-    char *tempSoundex = soundex;
-    int sIndex = 1;
-    // printf("\n%s", soundex);
-    for (int i = 1; i < strlen(tempSoundex); i++) {
-     if(tempSoundex[i] != tempSoundex[i-1]) {
-        soundex[sIndex++] = tempSoundex[i];
-        // printf("\n%s", soundex);
-     }   
-    }
-    
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+    int len = strlen(soundex);
+    for (int i = 1; i < len; ++i) {
+        if((strchr("0*", soundex[i]))) {
+            deleteCharFromStringByIndex(soundex,i);
+        }
     }
 }
 
 void generateSoundex_AddZeroPadding(char *soundex) {
     int len = strlen(soundex);
-    
     while(len < 4) {
-        soundex[len] = 0;
-        len++;
+        soundex[len++] = '0';
     }
 }
 
 void generateSoundex(const char *name, char *soundex) {
     generateSoundex_ReplaceCharacters(name, soundex);
-    // printf(soundex);
+    generateSoundex_SimplifyAdjacentCharacters_CheckForHW(soundex);
+    generateSoundex_SimplifyAdjacentCharacters_CheckForRepeatedCharacters(soundex);
     generateSoundex_RemoveVowels(soundex);
-    // printf(soundex);
-    generateSoundex_SimplifyAdjacentCharacters(soundex);
-    // printf(soundex);
-    // generateSoundex_AddZeroPadding(soundex);
-    // printf(soundex);
+    generateSoundex_AddZeroPadding(soundex);
+    soundex[0] = toupper(name[0]);
     soundex[4] = '\0';
-    printf(soundex);
     
 }
 
