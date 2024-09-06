d9500e97aceda6a6a82866ac9ce560d85d1c1c27
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-06 14:46:05 +0530

    Update Soundex.Tests.cpp
    uncommented assert statement

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,7 +5,7 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
-  //ASSERT_EQ(soundex,"A200");
+  ASSERT_EQ(soundex,"A200");
 }
 
 // TEST(SoudexTestsuite, DropsAllVowels) {
