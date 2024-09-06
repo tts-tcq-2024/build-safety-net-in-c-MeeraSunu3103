1a412bbe87379f2f78fabe26d31e23dce8683497
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-06 16:32:12 +0530

    Update Soundex.Tests.cpp
    Added Testcases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,15 +1,41 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
+char soundex[5], char soundex1[5], soundex2[5];
+
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
-  EXPECT_EQ(soundex,"A200");
+  generateSoundex("Robert", soundex);
+  ASSERT_EQ(soundex,"R163");
+  generateSoundex("Ruperd", soundex1);
+  ASSERT_EQ(soundex1,"R163");
+  ASSERT_EQ(soundex,soundex1);
+
+  generateSoundex("Honeyman", soundex2);
+  ASSERT_EQ(soundex2,"H555");
 }
 
-// TEST(SoudexTestsuite, DropsAllVowels) {
-//   char soundex[5];
-//   generateSoundex("Honeyman", soundex);
-//   ASSERT_EQ(soundex,"H555");
-// }
+TEST(SoudexTestsuite, DropsAllVowels) {
+  generateSoundex("Cameroon", soundex);
+  ASSERT_EQ(soundex,"C565");
+  generateSoundex("Cmrn", soundex1);
+  ASSERT_EQ(soundex1,"C565");
+  ASSERT_EQ(soundex,soundex1);
+}
+
+TEST(SoudexTestsuite, DropsRepeatedAdjacentEncodings) {
+  generateSoundex("Mississippi", soundex);
+  ASSERT_EQ(soundex,"M221");
+  generateSoundex("Misisipi", soundex1);
+  ASSERT_EQ(soundex1,"M221");
+  ASSERT_EQ(soundex,soundex1);
+
+  generateSoundex("Pfister", soundex2);
+  ASSERT_EQ(soundex2,"P236");
+}
+
+TEST(SoudexTestsuite, DropsRepeatedAdjacentEncodingsSeparatedByHOrW) {
+  generateSoundex("Vanyn", soundex);
+  ASSERT_EQ(soundex,"V550");
+  generateSoundex("Vamyn", soundex);
+  ASSERT_EQ(soundex,"V550");
+}
